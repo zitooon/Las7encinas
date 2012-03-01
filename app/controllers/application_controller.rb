231db298f46b9b20e5c9c8998b2e051c16995d86
zitooon <olivier.derobert@gmail.com>
2012-03-01 13:48:16 +0100

    get rid of loggers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
   private
   def default_url_options(options={})
-    logger.info 'salope'
-    {:locale => I18n.locale, :tarace => 'lapute'}
+    # logger.info 'salope'
+    {:locale => I18n.locale}#, :tarace => 'lapute'}
   end
   
   def is_logged
